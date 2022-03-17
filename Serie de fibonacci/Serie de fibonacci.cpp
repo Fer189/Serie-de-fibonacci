3b4d53034a331874a11f2f0033e7fb65f2ca7424
ferna <ferna@AORUSB550M>
2022-03-17 16:04:41 -0600

    Agregar archivos de proyecto.

---
+++ Serie de fibonacci/Serie de fibonacci.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numerosACalcular;
+    cout << "Secuencia de fibonacci" << endl;
+    cout << "Ingrese la cantidad de numeros que desea" << endl;
+    cin >> numerosACalcular;
+    for (int i = 0; i == numerosACalcular; i++) {
+
+    }
+}
