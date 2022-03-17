997e00c355795b79167f53b0b781da1585856178
FERNANDO ISMAEL NAVARRETE RUIZ <a21310393@live.ceti.mx>
2022-03-17 16:20:09 -0600

    Final commit

--- Serie de fibonacci/Serie de fibonacci.cpp
+++ Serie de fibonacci/Serie de fibonacci.cpp
@@ -3,11 +3,16 @@
 using namespace std;
 
 int main() {
+    int a = 0;
+    int b = 1;
     int numerosACalcular;
     cout << "Secuencia de fibonacci" << endl;
     cout << "Ingrese la cantidad de numeros que desea" << endl;
     cin >> numerosACalcular;
-    for (int i = 0; i == numerosACalcular; i++) {
-
+    cout << a << endl << b << endl;
+    for (int i = 3; i <= numerosACalcular; i++) {
+        cout << a + b << endl;
+        b = a + b;
+        a = b - a;
     }
 }
